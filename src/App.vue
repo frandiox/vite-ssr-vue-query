e61098ef235339620a0bfd5b1bb79762438273d4
Fran Dios <frankdiox@gmail.com>
2021-09-19 21:59:05 +0900

    Simplify integration

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, inject } from "vue";
 import { useQueryProvider } from "vue-query";
-import { useContext } from "vite-ssr/vue";
 import { VueQueryDevTools } from "vue-query/devtools";
 
 export default defineComponent({
   name: "App",
   components: { VueQueryDevTools },
   setup() {
-    const { initialState } = useContext();
-    useQueryProvider(initialState.vueQuery);
+    useQueryProvider(inject("vueQueryClient"));
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,20 @@ import routes from "./routes";
 import viteSSR from "vite-ssr/vue";
 import { QueryClient, hydrate, dehydrate } from "vue-query";
 
-export default viteSSR(
-  App,
-  {
-    routes,
-    transformState(state, defaultTransformer) {
-      if (import.meta.env.SSR) {
-        // Dehydrate client cache in server
-        state.vueQuery = dehydrate(state.vueQuery);
-      }
+export default viteSSR(App, { routes }, ({ app, initialState }) => {
+  // Create a new VueQuery client inside the main hook (once per request)
+  const client = new QueryClient();
+  // Since VueQuery does not support plugin-like installation,
+  // we must provide it manually so it can be used later in the app root.
+  app.provide("vueQueryClient", client);
 
-      return defaultTransformer(state);
-    }
-  },
-  ({ app, initialState }) => {
-    // Create VueQuery client
-    const client = new QueryClient();
-    // Hydrate existing state in browser
-    if (!import.meta.env.SSR) hydrate(client, initialState.vueQuery);
-    // Save reference to the client
-    initialState.vueQuery = client;
+  // Sync initialState with the client cache:
+  if (import.meta.env.SSR) {
+    // This is a placeholder that will return the VueQuery state during SSR.
+    // See how JSON.stringify works:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#description
+    initialState.vueQueryState = { toJSON: () => dehydrate(client) };
+  } else {
+    hydrate(client, initialState.vueQueryState);
   }
-);
+});
